3d10f0231d41c0b652beebcd5e38e83613a439ba
Tashi-N <12190086.gcit@rub.edu.bt>
2021-05-07 09:36:21 -0400

    initial commit

---
+++ SherigCare/app/src/main/java/edu/gcit/sherigcare/ui/slideshow/parents.java
@@ -0,0 +1,14 @@
+package edu.gcit.sherigcare.ui.slideshow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class parents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parents);
+    }
+}
