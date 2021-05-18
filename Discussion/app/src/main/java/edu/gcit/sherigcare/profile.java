121c8a6e6d4c977708d1d464c85fdbc00d340f1d
tsheringgyeltshen <12190086.gcit@rub.edu.bt>
2021-05-18 12:09:58 -0400

    TODO

--- Discussion/app/src/main/java/edu/gcit/sherigcare/profile.java
+++ Discussion/app/src/main/java/edu/gcit/sherigcare/profile.java
@@ -1,2 +1,14 @@
-package edu.gcit.sherigcare;public class profile {
+package edu.gcit.sherigcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
 }
